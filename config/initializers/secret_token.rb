1a6aef77ce731b7296532122a41d861d2f32a403
Miguel Barcos <miguel@galar.com>
2017-05-12 20:44:02 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecetarioApi::Application.config.secret_key_base = '4cbde8c78c0ab0ae3e47de4c1d96cd3813dd77a5b2e113223f19c660ac82a604b5dc9a77e65b2401ea230c0e6e7ff80ceae23ec10c03e4cc018ba5cc5d60ec62'
